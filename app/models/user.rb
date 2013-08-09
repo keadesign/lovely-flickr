bc5e1d26b758f4b034ee48f000e2bf62442eccc8
vagrant <vagrant@precise64.(none)>
2013-08-09 15:16:37 +0000

    add photo and login controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  # add this for passwords and enable bcrypt-ruby in gemfile
+  has_secure_password
+
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
 end
